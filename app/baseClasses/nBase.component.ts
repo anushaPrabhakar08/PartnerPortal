97ea7a46aa9b8860b995b2805a8d4f189cb87526
developer-geekay <developergeekay@gmail.com>
2019-11-11 22:34:59 +0530

    changes

--- app/baseClasses/nBase.component.ts
+++
@@ -1,8 +0,0 @@
-// Base component 
-import { NDataModel } from './nDataModel.class';
-export class NBaseComponent {
-    dm: NDataModel;
-    constructor() {
-        this.dm = new NDataModel();
-    }
-}
--- app/defaults/default.service.ts
+++
@@ -1,7 +0,0 @@
-/*DEFAULT GENERATED TEMPLATE. DO NOT CHANGE CLASS NAME*/
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class %className% {
-
-}
